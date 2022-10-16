1ef92cabcb86686f7defcf6f21025210088837d5
Abhinav-Thakur <abhinav.thakur1985@gmail.com>
2022-10-16 23:12:44 +0530

    New CK

--- app/src/main/java/com/moengageassignment/activities/MainActivity.java
+++ app/src/main/java/com/moengageassignment/activities/MainActivity.java
@@ -1,26 +1,23 @@
 package com.moengageassignment.activities;
 
-import android.os.AsyncTask;
+
 import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import androidx.lifecycle.Observer;
+
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.moengageassignment.R;
-import com.moengageassignment.apiservices.NewsListAsync;
+
 import com.moengageassignment.databinding.ActivityMainBinding;
-import com.moengageassignment.models.Article;
-import com.moengageassignment.models.News;
+
 import com.moengageassignment.viewmodels.NewsListModel;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding activityMainBinding;
--- app/src/main/java/com/moengageassignment/adapter/ArticleAdapter.java
+++ app/src/main/java/com/moengageassignment/adapter/ArticleAdapter.java
@@ -3,8 +3,6 @@ package com.moengageassignment.adapter;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,7 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 import androidx.databinding.DataBindingUtil;
 import androidx.navigation.Navigation;
